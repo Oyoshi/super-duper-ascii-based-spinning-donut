c34ddb7d867eecfdf39112eeb816766da9ca3159
Oyoshi <rafal.apriasz@gmail.com>
2022-11-24 13:40:07 +0100

    refactor magic numbers into constants

--- donut.py
+++ donut.py
@@ -2,47 +2,67 @@ from argparse import ArgumentParser, HelpFormatter
 from math import sin, cos, pi
 
 
-delta_theta = 0.07
-delta_phi = 0.02
+########################## Constants ##################################
+
+DELTA_THETA = 0.07
+DELTA_PHI = 0.02
 
 R1 = 1
 R2 = 2
 K2 = 5
 
-screen_width = 64
-screen_height = 64
+SCREEN_WIDTH = 64
+SCREEN_HEIGHT = 64
 
-K1 = screen_width * K2 * 3 / (8 * (R1 + R2))
+K1 = SCREEN_WIDTH * K2 * 3 / (8 * (R1 + R2))
+
+A0 = 1.0
+DELTA_A = 0.08
+B0 = 1.0
+DELTA_B = 0.03
 
 SYMBOLS = ".,-~:;=!*#$@"
-COLOURS = {"magenta": range(197, 201), "green": range(154, 158), "blue": range(24, 28)}
+
+COLOUR_SHADES = 4
+PINK_START_VAL = 204
+MAGENTA_START_VAL = 197
+GREEN_START_VAL = 154
+BLUE_START_VAL = 24
+COLOURS = {
+    "pink": range(PINK_START_VAL, PINK_START_VAL + COLOUR_SHADES),
+    "magenta": range(MAGENTA_START_VAL, MAGENTA_START_VAL + COLOUR_SHADES),
+    "green": range(GREEN_START_VAL, GREEN_START_VAL + COLOUR_SHADES),
+    "blue": range(BLUE_START_VAL, BLUE_START_VAL + COLOUR_SHADES),
+}
 
 
 def simulate_donut(A, B, colour):
     print("\x1b[2J")
     while True:
         render_frame(A, B, colour)
-        A += 0.08
-        B += 0.03
+        A += DELTA_A
+        B += DELTA_B
 
 
 def render_frame(A, B, colour):
     char_output, colour_output = compute_frame(A, B, colour)
 
     print("\x1b[H")
-    for i in range(screen_height):
-        for j in range(screen_width):
-            if not colour:
-                print(char_output[i][j], end="")
-            else:
-                print(f"\033[38;5;{colour_output[i][j]}m{char_output[i][j]}", end="")
+    for i in range(SCREEN_HEIGHT):
+        for j in range(SCREEN_WIDTH):
+            print(
+                f"\033[38;5;{colour_output[i][j]}m{char_output[i][j]}"
+                if colour
+                else char_output[i][j],
+                end="",
+            )
         print()
 
 
 def compute_frame(A, B, colour):
-    char_output = [[" " for _ in range(screen_width)] for _ in range(screen_height)]
-    colour_output = [[" " for _ in range(screen_width)] for _ in range(screen_height)]
-    zbuffer = [[0 for _ in range(screen_width)] for _ in range(screen_height)]
+    char_output = [[" " for _ in range(SCREEN_WIDTH)] for _ in range(SCREEN_HEIGHT)]
+    colour_output = [[" " for _ in range(SCREEN_WIDTH)] for _ in range(SCREEN_HEIGHT)]
+    zbuffer = [[0 for _ in range(SCREEN_WIDTH)] for _ in range(SCREEN_HEIGHT)]
 
     sinA, cosA = sin(A), cos(A)
     sinB, cosB = sin(B), cos(B)
@@ -50,12 +70,12 @@ def compute_frame(A, B, colour):
     theta = 0
     while theta < 2 * pi:
         sintheta, costheta = sin(theta), cos(theta)
-        theta += delta_theta
+        theta += DELTA_THETA
 
         phi = 0
         while phi < 2 * pi:
             sinphi, cosphi = sin(phi), cos(phi)
-            phi += delta_phi
+            phi += DELTA_PHI
 
             circle_x, circle_y = R2 + R1 * costheta, R1 * sintheta
 
@@ -70,8 +90,8 @@ def compute_frame(A, B, colour):
             z = K2 + cosA * circle_x * sinphi + circle_y * sinA
             ooz = 1 / z
 
-            xp, yp = int(screen_width / 2 + K1 * ooz * x), int(
-                screen_height / 2 - K1 * ooz * y
+            xp, yp = int(SCREEN_WIDTH / 2 + K1 * ooz * x), int(
+                SCREEN_HEIGHT / 2 - K1 * ooz * y
             )
 
             luminance = (
@@ -94,7 +114,10 @@ def compute_char(luminance, colour):
     luminance_index = (int)(luminance * 8)
     if not colour:
         return SYMBOLS[luminance_index], None
-    return SYMBOLS[luminance_index], COLOURS[colour][luminance_index // 3]
+    return (
+        SYMBOLS[luminance_index],
+        COLOURS[colour][luminance_index // (COLOUR_SHADES - 1)],
+    )
 
 
 def create_args_parser():
@@ -107,8 +130,8 @@ def create_args_parser():
         "-c",
         "--colour",
         type=str,
-        choices=["magenta", "green", "blue"],
-        metavar=f"['magenta', 'green', 'blue']",
+        choices=["pink", "magenta", "green", "blue"],
+        metavar=f"['pink', 'magenta', 'green', 'blue']",
         help="Colours palette",
     )
     return args_parser
@@ -117,5 +140,4 @@ def create_args_parser():
 if __name__ == "__main__":
     args_parser = create_args_parser()
     options = args_parser.parse_args()
-    A, B = 1.0, 1.0
-    simulate_donut(A, B, options.colour)
+    simulate_donut(A0, B0, options.colour)
